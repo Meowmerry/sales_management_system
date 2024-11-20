067b3a66024ee79da6bb20306dbfb4b7a9af6969
Cheer Chokwittaya <chokwittaya.nutt@gmail.com>
2024-11-19 23:22:31 -0600

    move files to frontend make webpack and babel works

---
+++ frontend/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-react", "@babel/preset-env"],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        modules: [
+            'node_modules',
+            // path.resolve(__dirname, 'frontend')
+        ],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/index.html'),
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, './public'),
+        },
+        port: 8080,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+        compress: true,
+    },
+    devtool: 'source-map',
+};

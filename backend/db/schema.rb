e5b06d85defbf1988eefd3c2fc8f0615a3df5484
meow3525@gmail.com <meow3525@gmail.com>
2024-11-21 07:28:45 -0600

    set up database;

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 202411163) do
+ActiveRecord::Schema[8.0].define(version: 202411163) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "order_items", force: :cascade do |t|
     t.bigint "order_id", null: false

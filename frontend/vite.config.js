dd7f51ed4e057edc5bb03dbc4a9cd5561c3bb465
meow3525@gmail.com <meow3525@gmail.com>
2024-11-22 16:07:01 -0600

    fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,13 @@ export default defineConfig({
     css: {
         postcss: "./postcss.config.js",
     },
+    build: {
+        outDir: "dist",
+        sourcemap: false,
+        minify: true,
+    },
+    server: {
+        host: true,
+        port: process.env.PORT || 3000,
+    },
 });
